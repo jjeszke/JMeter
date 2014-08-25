662bc4bc95166e3f1b32534b10f882359cdcfcbc
jjeszke <jakub.jeszke@gmail.com>
2014-08-25 18:31:13 +0200

    Dodanie opisow do requestow

--- index.py
+++ index.py
@@ -7,16 +7,21 @@ import math
 import random
 from datetime import timedelta, date
 PI = 3.14
+IP= "D001000"
 
 class jmeter(object):
+
     @cherrypy.expose
     def index(self):
-        return """<b>To jest prosta aplikacji, dzięki której nauczysz się podstaw JMetera.</b> <br><br> Pierwsze zadanie składa się z 4 requestów,  \
-               które musisz przetestować ("obwod", "pole, "numer-3" i "numer-4").
-               <br> Aby zobaczyć krótką dokumentację dotyczącą każdego z requestów, wykonanaj go bez podania argumentu na przykład "ip:8080/obwod"
-               <br> Trzeci i czwarty request to POSTy, więc aby poznać treść dokumentacji powinieneś skorzystać z samego JMetera (oczywiście da się
-               wykonać request POST z poziomu przeglądarki, ale celem zadania jest wykonanie go z poziomu Jmetera).
-               <br><br><b>POWODZENIA</b>"""
+        return """<b>To jest prosta aplikacji, dzięki której nauczysz się podstaw JMetera.</b> <br><br> Zadanie na dzisiaj
+            składa się z 4 requestów, które musisz przetestować, ich nazwy to "obwod", "pole, "data" i "numer-4". <br> Aby zobaczyć
+            krótką dokumentację dotyczącą każdego z requestów, wykonanaj go bez podania argumentu, na przykład
+            "{}:8080/obwod". <br> Pierwsze dwa requesty ("obwod" i "pole") to requesty typu GET. W celu zaznajomienia się
+            z ich dokumentacją, wykonaj je z poziomu paska adresu jakiejkolwiek przeglądarki. <br> Trzeci i czwarty
+            request ("data" i "numer-4") to requesty typu POST. Z wykorzystaniem odpowiednich dodatków je również możesz
+            wykonać z poziomu przeglądarki, ale proszę Cię abyś wykorzystał do tego samego JMetera. <br><br>
+            Celem dzisiejszego workshopu jest przetestowanie czasu odpowiedzi, kodu statusu HTML oraz samej odpowiedzi
+            wszystkcih czterech requestów.<br><br><b>POWODZENIA</b>""".format(IP)
 
     @cherrypy.expose
     def obwod(self, radius = None):
@@ -25,8 +30,12 @@ class jmeter(object):
         """
         try:
             if radius is None:
-                return """<B> To jest pierwsze zadanie workshopu o JMeterze.
-                """
+                return """<b>Request GET, numer 1.</b><br> Nazwa argumentu, którą trzeba podać do obliczenia pola koła jest "radius" i jego wartość musi on być większa od 0.
+                <br><br>Na początek spróbuj w pasku adresu przeglądarki wpisać dane requestu wraz z argumentem, niedługo później powinieneś zobaczyć odpowiedź.
+                <br><br>Przykładowy request:<b> http://{}:8080/obwod?radius=5 </b>
+                <br> Odpowiedź:<b> Obwod wynosi: 31.4 </b>
+                <br><br>Teraz czas podany request przetestować z wykorzystaniem JMetera - pamiętaj by przygotować asercję sprawdzającą odpowiedź (czy jest zgodna z przykładem).
+                """.format(IP)
             if float(radius) > 0:
                 return "Obwod wynosi: {}".format(str(2*PI*float(radius)))
             else:
@@ -41,7 +50,9 @@ class jmeter(object):
         """
         try:
             if promien is None:
-                return """<B> To jest pierwsze zadanie workshopu o JMeterze. </b><br>Ponizej znajdziesz szczegolowe informacje. <br>POWODZENIA!"""
+                return """<b>Request GET, numer 2.</b><br><br> Nazwa argumentu, którą trzeba podać do obliczenia pola koła jest "promien" i jego wartość musi on być większa od 0.
+                <br><br>Przykładowa odpowiedź:<b> Pole wynosi: 12.56 </b>
+                <br><br>Tak jak przy poprzednim zadaniu, przetestuj ten request z wykorzystaniem JMetera (pamiętaj o wszystkich asercjach)!"""
             if float(promien) > 0:
                 time.sleep(10)
                 return "Pole wynosi: {}".format(str(PI*math.pow(float(promien),2)))
@@ -58,10 +69,14 @@ class jmeter(object):
         """
         try:
             if dni is None:
-                return """<B> Trzecie zadanie: Prawidłowy format daty: 2014-09-01"""
+                return """<b>Request POST, numer 3.</b><br><br> Request zwraca datę dzisiejszą powiększoną o ilości dni podaną w argumencie.
+                Nazwa tego argumentu to po prostu "dni" i przyjmuje wartości całkowite większe od 0
+                <br> Odpowiedź powinna zostać zwrócona w formacie: YYYY-MM-DD (na przykład: 2014-09-15)
+                <br><br>Tak jak przy poprzednich zadaniach, przetestuj ten request z wykorzystaniem JMetera (pamiętaj o wszystkich asercjach)
+                """
             if int(dni) > 0:
-                time.sleep(random.choice([1,2,3,7,8,9]))
-                return str(date.today() + timedelta(days = int(dni)+10))
+                time.sleep(random.choice([1,2,3,8,9,10]))
+                return str(date.today() + timedelta(days = int(dni) + 10))
             else:
                 return "Na pewno zrobiles wszystko dobrze?"
         except:
